cee941d618d36db489cc93c736ab70cb2019eca6
Ormon-huehuehue <armnvrma10@gmail.com>
2025-01-01 18:20:40 +0530

    error handling in rust

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
+use std::fs;
+
+
 fn main(){
-    let index = find_first_a(String::from("jella"));
+    let result = fs::read_to_string("hello.txt");
 
-    match index{
-        Some(i) => println!("The index of first a is {}", i),
-        None => println!("There is no a in the string")
-    }
-}
-
-fn find_first_a(s : String)->Option<i32>{
-    for(index, char) in s.chars().enumerate(){
-        if char=='a'{
-            return Some(index as i32);
+    match result{
+        Ok(content)=>{
+            println!("File content : {}", content);
+        }
+        Err(error)=>{
+            println!("Error reading file : {}", error);
         }
     }
-
-    return None;
 }
