b2a06820b8bf74aadce3340a2e27072380b8d73e
Ormon-huehuehue <armnvrma10@gmail.com>
2025-01-01 16:23:43 +0530

    another one

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
-fn main(){
-  
+struct User {
+    first_name : String,
+    last_name  : String,
+    age : u8
+}
 
+
+
+fn main(){
     let name = String::from("Armaan");
     let len = get_string_length(name);
 
-    println!("{}", len);
+    println!("Lenght of name {}", len);
 
+    let user = User{
+        first_name : String::from("Armaan"),
+        last_name : String::from("Verma"),
+        age : 18
+    };
+
+
+    println!("first name : {}", user.first_name);
+    println!("last name : {}", user.last_name);
+    println!("age : {}", user.age);
 }
+
+
+
 
 
 fn get_string_length(s : String) -> usize {
