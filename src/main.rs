10840bb236c5576129fc8ae44d01e8ff67f843d6
Ormon-huehuehue <armnvrma10@gmail.com>
2025-01-01 18:01:46 +0530

    options null

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,18 @@
-// struct User {
-//     first_name : String,
-//     last_name  : String,
-//     age : u8
-// }
-
-// struct Rect{
-//     width : u32,
-//     height : u32
-// }
-
-// impl Rect{
-//     fn area(&self)-> u32{
-//         return self.width * self.height;
-//     }
-// }
-
-enum Direction{
-    Up(String, f64), 
-    Down(String, f64)
-}
-
 fn main(){
-   let new_direction = Direction::Up(String::from("Up"), 30.0);
-   let _down_direction: Direction = Direction::Down(String::from("Down"), 40.0);
-   
+    let index = find_first_a(String::from("jella"));
 
-   match new_direction{
-    Direction::Up(direction, distance)=>{
-        println!("Moving {}km in {} direction", distance, direction);
+    match index{
+        Some(i) => println!("The index of first a is {}", i),
+        None => println!("There is no a in the string")
     }
-
-    Direction::Down(direction, distance)=>{
-        println!("Moving {}km in {} direction", distance, direction);
-    }
-   }
 }
 
+fn find_first_a(s : String)->Option<i32>{
+    for(index, char) in s.chars().enumerate(){
+        if char=='a'{
+            return Some(index as i32);
+        }
+    }
 
-
-
-
-
-// fn get_string_length(s : String) -> usize {
-//     s.chars().count()
-// }
-
-
-// fn is_even(num : i32) -> bool {
-//     if num % 2 == 0 {
-//         return true;
-//     }
-//     else{
-//         return false;
-//     }
-// }
-
-// fn fib(num : i32)-> i32 {
-//     let mut first = 0;
-//     let mut second = 1;
-
-//     println!("num entered  : {}", num);
-//     if num == 0 {
-//         return first;
-//     }
-//     if num ==1 {
-//         return 1;
-//     }
-
-//     for _i in 2..=num {
-    
-//          let temp = second;
-//          second =  second + first;
-//          first = temp;
-//     } 
-
-//     return second;
-// }
+    return None;
+}
