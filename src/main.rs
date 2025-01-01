8598598a8144a6e81e5e116a6a767a951d0ea515
Ormon-huehuehue <armnvrma10@gmail.com>
2025-01-02 00:03:37 +0530

    hashmaps

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,16 @@
+use std::collections::HashMap;
+
 fn main(){
-    let mut vec = Vec::new();
+    let mut users = HashMap::new();
+    
+    users.insert(String::from("armaan") , 21);
+    users.insert(String::from("ormon"), 3 );
 
-    vec.push(1);
-    vec.push(2);
-    vec.push(3);
-    vec.push(4);
+    let age_of_user = users.get("ormon");
 
-    even_values(&mut vec); // passing in a mutable reference to the vector
-
-    println!("Updated vector : {:?}", vec);
-}
-
-fn even_values(v : &mut Vec<i32>){
-
-    let mut i = 0 ;
-
-    while i < v.len(){
-        if v[i] % 2 != 0{
-            v.remove(i);
-        }else{
-            i += 1;
-        }
+    match age_of_user{
+        Some(number) => println!("age of user: {}", number),
+        None => println!("User not found")
     }
+
 }
