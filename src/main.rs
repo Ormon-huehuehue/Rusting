a0733dac26c01d70b8d34680269f3ce9c14eb783
Ormon-huehuehue <armnvrma10@gmail.com>
2025-01-01 16:55:44 +0530

    struct implementation

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,53 @@
-struct User {
-    first_name : String,
-    last_name  : String,
-    age : u8
+// struct User {
+//     first_name : String,
+//     last_name  : String,
+//     age : u8
+// }
+
+struct Rect{
+    width : u32,
+    height : u32
 }
 
+impl Rect{
+    fn area(&self)-> u32{
+        return self.width * self.height;
+    }
+}
 
 
 fn main(){
-    let name = String::from("Armaan");
-    let len = get_string_length(name);
+    // let name = String::from("Armaan");
+    // let len = get_string_length(name);
 
-    println!("Lenght of name {}", len);
+    // println!("Lenght of name {}", len);
 
-    let user = User{
-        first_name : String::from("Armaan"),
-        last_name : String::from("Verma"),
-        age : 18
+    // let user = User{
+    //     first_name : String::from("Armaan"),
+    //     last_name : String::from("Verma"),
+    //     age : 18
+    // };
+
+
+    // println!("first name : {}", user.first_name);
+    // println!("last name : {}", user.last_name);
+    // println!("age : {}", user.age);
+
+    let rect1 = Rect {
+        width:  30,
+        height : 30
     };
 
-
-    println!("first name : {}", user.first_name);
-    println!("last name : {}", user.last_name);
-    println!("age : {}", user.age);
+    println!("Area of rectangle : {}", rect1.area())
 }
 
 
 
 
 
-fn get_string_length(s : String) -> usize {
-    s.chars().count()
-}
+// fn get_string_length(s : String) -> usize {
+//     s.chars().count()
+// }
 
 
 // fn is_even(num : i32) -> bool {
