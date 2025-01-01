0388b0fc050ffec102552053566a1f712d98d82f
Ormon-huehuehue <armnvrma10@gmail.com>
2025-01-01 16:09:56 +0530

    fib

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
 fn main(){
-    println!("{}", is_even(3))
+    // println!("{}", is_even(3));
+    println!("{}", fib(5));
 }
 
-fn is_even(num : i32) -> bool {
-    if num % 2 == 0 {
-        return true;
+// fn is_even(num : i32) -> bool {
+//     if num % 2 == 0 {
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+fn fib(num : i32)-> i32 {
+    let mut first = 0;
+    let mut second = 1;
+
+    println!("num entered  : {}", num);
+    if num == 0 {
+        return first;
     }
-    else{
-        return false;
+    if num ==1 {
+        return 1;
     }
+
+    for _i in 2..=num {
+    
+         let temp = second;
+         second =  second + first;
+         first = temp;
+    } 
+
+    return second;
 }
