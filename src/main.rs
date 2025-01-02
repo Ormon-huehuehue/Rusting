56d7387c6b781f81d6391dedbb938840131ab0cd
Ormon-huehuehue <armnvrma10@gmail.com>
2025-01-02 13:38:31 +0530

    iterators

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,9 @@
-use std::collections::HashMap;
-
-fn group_values_by_key(vec : Vec<(String, i32)>)-> HashMap<String, i32>{
-    let mut hm = HashMap::new();
-
-    for (key, value) in vec{
-        hm.insert(key, value);
-    }
-
-    return hm;
-
-}
-
 fn main(){
-   let pairs = vec![
-    (String::from("Armaan"),1),
-    (String::from("Ormon"), 2)
-   ];
+	let nums = vec![1,2,3];
+	let mut iter = nums.iter();
 
-   println!("hashmap obtained :-");
-
-   for (key, value) in group_values_by_key(pairs){
-    println!("{} : {:?}", key, value);
-   }
-
+    while let Some(value) = iter.next() {
+        println!("{}", value);
+    }
+    println!("{:?}", nums);
 }
