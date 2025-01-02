169df6c5f7f9420fd1c1aedfb81ec655db244ce7
Ormon-huehuehue <armnvrma10@gmail.com>
2025-01-02 17:29:53 +0530

    traits as parameters

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
-fn main(){
-    let word = "ormon verma";
-    let word2 = find_first_word(word);
-    
-    println!("{}", word);
-    println!("{}", word2);
-
+trait Summary{
+    fn summarize(&self)->String{
+        return String::from("Summary")
+    }
 }
 
-fn find_first_word(word : &str)-> &str{
-    let mut index= 0 ;
-    for (_, i) in word.chars().enumerate() {
-        if i == ' '{
-            break;
-        }
-        index = index+1;
-    }
+struct Fix{}
+impl Summary for Fix{}
 
-    return &word[0..index];
+fn main(){
+
+    let fix : Fix = Fix{};
+
+	notify(fix)
+}
+
+fn notify(u : impl Summary){
+	println!("{}", u.summarize());
 }
