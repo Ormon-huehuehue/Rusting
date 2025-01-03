cf957b75eba169474298647c9a834253a9035fc1
Ormon-huehuehue <armnvrma10@gmail.com>
2025-01-03 20:47:16 +0530

    random program

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,28 @@ use std::thread;
 fn main(){
 	let (tx, rx) = mpsc::channel();
 
-	thread::spawn(move || {
-		let val = String::from("hi");
-		tx.send(val).unwrap();
-	});
+	for i in 0..8 {
+        let producer = tx.clone();
 
-	let received = rx.recv().unwrap();
-	println!("Got : {received}");
+        thread::spawn(move || {
+            let mut sum = 0;
+            for j in i*1000..(i+1)*1000-1{
+                sum +=j;
+            }
+
+            producer.send(sum);
+        });
+
+    }
+
+    drop(tx);  //drop tx so that the receiver can know that there is no more data to receive
+
+
+    let mut final_sum = 0;
+    for value in rx{
+        println!("Received: {}", value);
+        final_sum += value;
+    }
+
+    println!("Final sum is: {}", final_sum);
 }
